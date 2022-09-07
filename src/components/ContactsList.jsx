31593df7cb85677cee36fe7ac9d24c2005a53f63
NatalaBoiko <natalabojko@gmail.com>
2022-09-07 19:10:19 +0300

    cl

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -6,63 +6,24 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { contactsOperations } from '../redux/contacts/contactsOperations';
 
-export const ContactsList = () => {
-  const { contacts, isLoaging, filter } = useContacts();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(contactsOperations.getContacts());
-  }, [dispatch]);
-
-  const findContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    if (contacts) {
-      return contacts.filter(contact =>
-        contact.name.toLowerCase().includes(normalizedFilter)
-      );
-    }
-  };
-
-  const filteredContacts = findContacts();
-
+export const ContactsList = ({ contacts }) => {
   return (
     <div>
-      {isLoaging ? (
-        <Loader />
-      ) : (
-        <ul className={s.items__container}>
-          {contacts &&
-            filteredContacts.map(({ id, name, number }) => {
-              return (
-                <li className={s.item} key={id}>
-                  <h3 className={s.item__name}>{name}:</h3>
-                  <p className={s.item__name}>{number}</p>
-                  <button className={s.user__btn} type="button">
-                    {/* {isLoaging ? '...' : 'Delete'} */}
-                    Delete
-                  </button>
-                </li>
-              );
-            })}
-        </ul>
-      )}
+      <ul className={s.items__container}>
+        {contacts &&
+          filteredContacts.map(({ id, name, number }) => {
+            return (
+              <li className={s.item} key={id}>
+                <h3 className={s.item__name}>{name}:</h3>
+                <p className={s.item__name}>{number}</p>
+                <button className={s.user__btn} type="button">
+                  {/* {isLoading ? '...' : 'Delete'} */}
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 };
-
-{
-  /* <ul className={s.items__container}>
-{contacts &&
-  filteredContacts.map(({ id, name, number }) => {
-    return (
-      <li className={s.item} key={id}>
-        <h3 className={s.item__name}>{name}:</h3>
-        <p className={s.item__name}>{number}</p>
-        <button className={s.user__btn} type="button">
-          {isLoading ? '...' : 'Delete'}
-        </button>
-      </li>
-    );
-  })}
-</ul> */
-}
