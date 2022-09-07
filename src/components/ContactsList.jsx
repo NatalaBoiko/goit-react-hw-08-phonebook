b8a8e54ee336b6a0ca26bb00040e91819bac98e1
NatalaBoiko <natalabojko@gmail.com>
2022-09-07 19:04:30 +0300

    contactList

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,39 +1,68 @@
 import s from '../Styles.module.css';
+import { Loader } from '../components/Loader/Loader';
 
-const contacts = [
-  // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+import { useContacts } from '../components/hooks';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { contactsOperations } from '../redux/contacts/contactsOperations';
+
 export const ContactsList = () => {
+  const { contacts, isLoaging, filter } = useContacts();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(contactsOperations.getContacts());
+  }, [dispatch]);
+
+  const findContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    if (contacts) {
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(normalizedFilter)
+      );
+    }
+  };
+
+  const filteredContacts = findContacts();
+
   return (
     <div>
-      {/* <h2>Contacts</h2> */}
-      <ul className={s.items__container}>
-        {contacts &&
-          contacts.map(({ id, name, number }) => {
-            return (
-              <li className={s.item} key={id}>
-                {/* <div className={s.item}> */}
-                <h3 className={s.item__name}>{name}:</h3>
-                <p className={s.item__name}>{number}</p>
-                {/* </div> */}
-                <button
-                  className={s.user__btn}
-                  type="button"
-                  // onClick={() => {
-                  //   dispatch(contactsOperations.deleteContact(id));
-                  //   dispatch(setFilter(''));
-                  // }}
-                >
-                  {/* {isLoading ? '...' : 'Delete'} */}
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-      </ul>
+      {isLoaging ? (
+        <Loader />
+      ) : (
+        <ul className={s.items__container}>
+          {contacts &&
+            filteredContacts.map(({ id, name, number }) => {
+              return (
+                <li className={s.item} key={id}>
+                  <h3 className={s.item__name}>{name}:</h3>
+                  <p className={s.item__name}>{number}</p>
+                  <button className={s.user__btn} type="button">
+                    {/* {isLoaging ? '...' : 'Delete'} */}
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+        </ul>
+      )}
     </div>
   );
 };
+
+{
+  /* <ul className={s.items__container}>
+{contacts &&
+  filteredContacts.map(({ id, name, number }) => {
+    return (
+      <li className={s.item} key={id}>
+        <h3 className={s.item__name}>{name}:</h3>
+        <p className={s.item__name}>{number}</p>
+        <button className={s.user__btn} type="button">
+          {isLoading ? '...' : 'Delete'}
+        </button>
+      </li>
+    );
+  })}
+</ul> */
+}
