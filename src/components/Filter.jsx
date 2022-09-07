53acb97525c9f8cab82b4b9ee89295ecf6ee4008
NatalaBoiko <natalabojko@gmail.com>
2022-09-07 17:22:03 +0300

    filterHooks

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,8 @@
 import s from '../Styles.module.css';
-import { changeFilter } from '../redux/contacts/contactsSlise';
-
-import { useSelector, useDispatch } from 'react-redux';
+import { useContacts } from '../components/hooks';
 
 export const Filter = () => {
-  const dispatch = useDispatch();
-  const filter = useSelector(state => state.contacts.filter);
-
+  const { filter, setFilter } = useContacts();
   return (
     <div className={s.cont__container}>
       <h2 className={s.home__title}>Filter</h2>
@@ -15,8 +11,30 @@ export const Filter = () => {
         name="filter"
         value={filter}
         placeholder="Find contacts by name"
-        onChange={e => dispatch(changeFilter(e.target.value))}
+        onChange={e => setFilter(e.target.value)}
       />
     </div>
   );
 };
+
+// import s from '../Styles.module.css';
+// import { changeFilter } from '../redux/contacts/contactsSlise';
+// import { useSelector, useDispatch } from 'react-redux';
+
+// export const Filter = () => {
+//   const dispatch = useDispatch();
+//   const filter = useSelector(state => state.contacts.filter);
+
+//   return (
+//     <div className={s.cont__container}>
+//       <h2 className={s.home__title}>Filter</h2>
+//       <input
+//         type="text"
+//         name="filter"
+//         value={filter}
+//         placeholder="Find contacts by name"
+//         onChange={e => dispatch(changeFilter(e.target.value))}
+//       />
+//     </div>
+//   );
+// };
--- src/components/hooks.js
+++ src/components/hooks.js
@@ -1,16 +1,17 @@
-// import { useSelector, useDispatch } from 'react-redux';
-// import { changeFilter } from '../redux/contacts/contactsSlise';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeFilter } from '../redux/contacts/contactsSlise';
 
-// export const useContacts = () => {
-//   const dispatch = useDispatch();
-//   const filter = useSelector(state => state.contacts.filter);
+export const useContacts = () => {
+  const dispatch = useDispatch();
 
-//   const setFilter = value => {
-//     console.log(value);
-//     dispatch(changeFilter(value));
-//   };
+  const filter = useSelector(state => state.contacts.filter);
 
-//   return {
-//     setFilter,
-//   };
-// };
+  const setFilter = value => {
+    dispatch(changeFilter(value));
+  };
+
+  return {
+    filter,
+    setFilter,
+  };
+};
