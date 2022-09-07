5531cbdd252201c636af7c5f5d19e33ae141f48f
NatalaBoiko <natalabojko@gmail.com>
2022-09-07 19:56:51 +0300

    cl2

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -49,25 +49,3 @@ export const ContactsList = () => {
     </div>
   );
 };
-
-// export const ContactsList = ({ contacts }) => {
-//   return (
-//     <div>
-//       <ul className={s.items__container}>
-//         {contacts &&
-//           contacts.map(({ id, name, number }) => {
-//             return (
-//               <li className={s.item} key={id}>
-//                 <h3 className={s.item__name}>{name}:</h3>
-//                 <p className={s.item__name}>{number}</p>
-//                 <button className={s.user__btn} type="button">
-//                   {/* {isLoading ? '...' : 'Delete'} */}
-//                   Delete
-//                 </button>
-//               </li>
-//             );
-//           })}
-//       </ul>
-//     </div>
-//   );
-// };
--- src/views/Contacts.jsx
+++ src/views/Contacts.jsx
@@ -2,37 +2,13 @@ import s from '../Styles.module.css';
 import { ContacstForm } from '../components/ContactsForm';
 import { Filter } from '../components/Filter';
 import { ContactsList } from '../components/ContactsList';
-// import { Loader } from '../components/Loader/Loader';
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { useContacts } from '../components/hooks';
-// import { contactsOperations } from '../redux/contacts/contactsOperations';
 
 const Contacts = () => {
-  // const dispatch = useDispatch();
-  // const { contacts, isLoaging, filter } = useContacts();
-
-  // useEffect(() => {
-  //   dispatch(contactsOperations.getContacts());
-  // }, [dispatch]);
-
-  // const findContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   if (contacts) {
-  //     return contacts.filter(contact =>
-  //       contact.name.toLowerCase().includes(normalizedFilter)
-  //     );
-  //   }
-  // };
-
-  // const filteredContacts = findContacts();
-
   return (
     <div className={s.view__container}>
       <h2 className={s.home__title}>Contacts</h2>
       <ContacstForm />
       <Filter />
-      {/* {isLoaging ? <Loader /> : <ContactsList contacts={filteredContacts} />} */}
       <ContactsList />
     </div>
   );
