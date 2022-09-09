d5e71c53cb2235528560dc0585b9c9299f26bb6f
NatalaBoiko <natalabojko@gmail.com>
2022-09-09 15:15:00 +0300

    cloned from -08-r

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook-r/">
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
           <App />
         </BrowserRouter>
       </PersistGate>
