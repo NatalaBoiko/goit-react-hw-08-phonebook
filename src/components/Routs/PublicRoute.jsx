44b63036a9ed79cd45ac80d1b470cd8ca79eca2e
NatalaBoiko <natalabojko@gmail.com>
2022-09-10 20:20:06 +0300

    Update PublicRoute.jsx

--- src/components/Routs/PublicRoute.jsx
+++ src/components/Routs/PublicRoute.jsx
@@ -2,10 +2,13 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import authSelectors from '../../redux/auth/auth-selectors';
 
-const PublicRoute = ({ children, restricted = false }) => {
+const PublicRoute = ({ children }) => {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  const shouldRedirect = isLoggedIn && restricted;
-  return shouldRedirect ? <Navigate to="/contacts" replace /> : children;
+  return isLoggedIn ? <Navigate to="/contacts" replace /> : children;
 };
 
 export default PublicRoute;
+
+// {children, restricted = false }
+// const shouldRedirect = isLoggedIn && restricted;
+// return shouldRedirect ? <Navigate to="/contacts" replace /> : children;
